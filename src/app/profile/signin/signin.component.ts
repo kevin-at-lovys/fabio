d186574765f0cfaf68dd9e577216c90129c129f0
Fábio Almeida <f.almeida.work@gmail.com>
2020-08-27 23:57:40 +0100

    Start desiging ui components

--- src/app/profile/signin/signin.component.ts
+++ src/app/profile/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { UserService } from '../user.service';
+
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService : UserService
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
 
-  constructor() { }
-
-  ngOnInit(): void {
   }
 
+
+  ngOnInit(): void {
+
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // reset alerts on submit
+    // this.alertService.clear();
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.userService.login(this.f.username.value, this.f.password.value)
+    .then(user => {
+      
+    }).catch(err =>{
+
+    });
+  }
 }
